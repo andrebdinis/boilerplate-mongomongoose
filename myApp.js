a4adbeab140392dec1c86b9b2902bd8b75a1449e
André Barreira Dinis <107752110+andrebdinis@users.noreply.github.com>
2022-06-20 16:57:34 +0000

    Added to package.json the packages: "mongodb": "~3.6.0", "mongoose": "~5.4.0"
    myApp.js:
    let mongoose = require("mongoose");
    
    Added a secret (environment variable): MONGO_URI (contains the connection to establish between my MongoDB cluster database and this app.
    
    Created variable of mongoose and connected mongodb and this app.
    
    BASH:
    $ npm install
    $ npm audit fix --force

--- myApp.js
+++ myApp.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+let mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGO_URI;
+// const mySecret = process.env['MONGO_URI']
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 let Person;
